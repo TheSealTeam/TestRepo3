286b7f46f90ecb00c8bca97c1648613b28afc004
Simon Olofsson <zigge112@gmail.com>
2021-07-18 12:24:50 +0200

    aes start code

---
+++ src/aes.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/* 
+ *          x
+ *          |
+ *   k -->[AES]
+ *          |
+ *          y
+ */
+
+/* 
+ *  key lengths |# rounds = n_r
+ *  ----------------------------
+ *     128 bits | 10
+ *     192 bits | 12
+ *     256 bits | 14
+ * 
+ */
+
+public void aes_enc(void)
+{
+
+}
+
+/*
+ *
+ * 
+ * 
+ */      
+public void aes_dec(void)
+{
+
+}
+
+public vodi aes_des(void)
+{
+
+}
+
+public void aes_dec()
+/*
+ *
+ * 
+ * 
+ */
